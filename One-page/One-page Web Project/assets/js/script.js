2aaa3d5d68b99ed1892017bba752b358897fb029
Šimon Novák <154249147+simiikqqq@users.noreply.github.com>
2025-12-03 13:22:59 +0100

    Update script.js

--- One-page/One-page Web Project/assets/js/script.js
+++ One-page/One-page Web Project/assets/js/script.js
@@ -1,1 +1,11 @@
+const buttons = document.querySelectorAll("nav button");
+const sections = document.querySelectorAll("main section");
 
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const target = button.dataset.section;
+
+        sections.forEach(s => s.classList.remove("active"));
+        document.getElementById(target).classList.add("active");
+    });
+});
