5e666cc79a24a50c57e02d565b8f783e9cceb017
Šimon Novák <154249147+simiikqqq@users.noreply.github.com>
2025-12-10 14:09:19 +0100

    Update script.js

--- One-page/One-page Web Project/assets/js/script.js
+++ One-page/One-page Web Project/assets/js/script.js
@@ -9,3 +9,30 @@ buttons.forEach(button => {
         document.getElementById(target).classList.add("active");
     });
 });
+
+
+async function loadNotes() {
+    const response = await fetch("backend.php?action=getNotes");
+    const notes = await response.json();
+    return notes;
+}
+
+async function saveNote(data) {
+    const response = await fetch("backend.php?action=saveNote", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
+    });
+
+    return await response.json();
+}
+
+async function deleteNote(id) {
+    const response = await fetch("backend.php?action=deleteNote&id=" + id);
+    return await response.json();
+}
+
+async function moveToArchive(id) {
+    const response = await fetch("backend.php?action=archiveNote&id=" + id);
+    return await response.json();
+}
